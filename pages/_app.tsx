7e7af3119a4ce1f93bf17672cf202fb8abf30e73
jjhstoday <jjh.s.today@gmail.com>
2023-07-27 16:34:14 +0900

    perf: 구글 애널리틱스 등록

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,29 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../seo.config';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      {/** GA https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-F91C6V7T4S`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F91C6V7T4S');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
